aa396b237551e7311b65e6f12941332a4987482e
Damarisdiazn <159404056+Damarisdiazn@users.noreply.github.com>
2024-06-30 18:47:27 -0500

    Tipos de datos, Identificadores
    base es de tipo float (número decimal) para permitir precisión en el cálculo.
    altura es de tipo integer (número entero), adecuado ya que la altura del triángulo generalmente se expresa como un número entero.
    area es de tipo float, ya que el resultado del cálculo del área puede ser un número decima

---
+++ Tipos de datos.py
@@ -0,0 +1,14 @@
+# Utiliza la fórmula: Área = (base * altura) / 2
+
+
+base = 10.5
+altura = 7
+
+# Cálculo del área utilizando la fórmula
+area = (base * altura) / 2
+
+# Imprimir el resultado del cálculo del área
+print("El área del triángulo con base", base, "y altura", altura, "es:", area)
+
+# La fórmula del área de un triángulo se obtiene multiplicando la base por la altura y dividiendo el resultado por 2.
+
